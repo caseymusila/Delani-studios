865d7a0cfd604dcdb5ac4711a0d8f2c4ad8c4f53
caseymusila <musilacasey@gmail.com>
2021-06-21 01:38:44 +0300

    create functions to various keys using jquery and javascript

---
+++ js/script.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+    $(".col-md-4").click(function(){
+        $(this).siblings.hide();
+    })
+    $(".picture").click(function(){
+        $(this).hide();
+        $(this).siblings(".define").show();
+    })
+  
+    $(".define").click(function(){
+        $(this).hide();
+        $(this).siblings(".picture").show();
+    })
+  })
+  
+  $(document).ready(function(){
+  $("#port1").mouseover(function(){
+    $("#display1").show();
+  }).mouseout(function(){
+    $("#display1").hide();
+  });
+  });
+  $(document).ready(function(){
+  $("#port2").mouseover(function(){
+    $("#display2").show();
+  }).mouseout(function(){
+    $("#display2").hide();
+  });
+  });
+  $(document).ready(function(){
+  $("#port3").mouseover(function(){
+    $("#display3").show();
+  }).mouseout(function(){
+    $("#display3").hide();
+  });
+  });
+  $(document).ready(function(){
+  $("#port4").mouseover(function(){
+    $("#display4").show();
+  }).mouseout(function(){
+    $("#display4").hide();
+  });
+  });
+  
+  $(document).ready(function(){
+  $("#port5").mouseover(function(){
+    $("#display5").show();
+  }).mouseout(function(){
+    $("#display5").hide();
+  });
+  $("#port6").mouseover(function(){
+    $("#display6").show();
+  }).mouseout(function(){
+    $("#display6").hide();
+  });
+  $("#port7").mouseover(function(){
+    $("#display7").show();
+  }).mouseout(function(){
+    $("#display7").hide();
+  });
+  $("#port8").mouseover(function(){
+    $("#display8").show();
+  }).mouseout(function(){
+    $("#display8").hide();
+  });
+  });
+  
